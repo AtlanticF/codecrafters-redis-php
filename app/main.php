de6e46f02490db96236bc16b7d8a07ed3056fb4b
gewen <gewen@zhumanggroup.com>
2024-06-27 11:24:09 +0800

    Update: support PING

--- app/main.php
+++ app/main.php
@@ -1,15 +1,15 @@
 <?php
 error_reporting(E_ALL);
 
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 // Since the tester restarts your program quite often, setting SO_REUSEADDR
 // ensures that we don't run into 'Address already in use' errors
-socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
-socket_bind($socket, 'localhost', 6379);
-socket_listen($socket, 5);
-socket_accept($socket); // Wait for first client
+socket_set_option($sock, SOL_SOCKET, SO_REUSEADDR, 1);
+socket_bind($sock, 'localhost', 6379);
+socket_listen($sock, 5);
+$socket = socket_accept($sock); // Wait for first client
 
-socket_write($socket, "+PONG\r\n", strlen("+PONG\r\n"));
+socket_write($socket, "+PONG\r\n");
 
-socket_close($socket);
+socket_close($sock);
 ?>
