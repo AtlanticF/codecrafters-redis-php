1aa48361c9872549904acc2914c054a6597417e6
gewen <gewen@zhumanggroup.com>
2024-07-01 13:28:24 +0800

    codecrafters submit [skip ci]

--- app/main.php
+++ app/main.php
@@ -3,11 +3,19 @@ require_once "protocol.php";
 
 error_reporting(E_ALL);
 
+$shortOptions = "p:"; // -p
+$longOptions = [
+    "port:", // --port
+];
+$options = getopt($shortOptions, $longOptions);
+// p or port => set port
+$port = $options['p'] ?? $options['port'] ?? 6379;
+
 $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 // Since the tester restarts your program quite often, setting SO_REUSEADDR
 // ensures that we don't run into 'Address already in use' errors
 socket_set_option($sock, SOL_SOCKET, SO_REUSEADDR, 1);
-socket_bind($sock, 'localhost', 6379);
+socket_bind($sock, 'localhost', $port);
 socket_listen($sock, 5);
 
 echo "Server started. Waiting for connections...\n";
